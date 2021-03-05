6b6caf27211e4fc61245af27ab2c79fe20cca6c4
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-03-05 11:28:12 +0530

    added exercise 1 - font props

---
+++ Exercise1/app/src/main/java/com/krish_the_dev/exercise1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.krish_the_dev.exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    float fontSize = 28;
+    int[] colors = {Color.RED, Color.BLUE, Color.MAGENTA, Color.GREEN, Color.YELLOW};
+    int currentColor = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView helloText = (TextView) findViewById(R.id.textView);
+        final Button changeFontButton = (Button) findViewById(R.id.fontButton);
+        final Button changeColorButton = (Button) findViewById(R.id.colorButton);
+
+        changeFontButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helloText.setTextSize(fontSize);
+                fontSize+=4;
+                if (fontSize == 52) { fontSize = 24; }
+            }
+        });
+
+        changeColorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helloText.setTextColor(colors[currentColor]);
+                currentColor += 1;
+                if (currentColor > colors.length - 1) { currentColor = 0; }
+            }
+        });
+    }
+}
