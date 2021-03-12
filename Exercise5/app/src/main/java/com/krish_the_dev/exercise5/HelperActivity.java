b5dd234d006bd4865e8248c6805085bd15e22f6c
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-03-12 17:59:58 +0530

    added ex 5 - notification creater

---
+++ Exercise5/app/src/main/java/com/krish_the_dev/exercise5/HelperActivity.java
@@ -0,0 +1,13 @@
+package com.krish_the_dev.exercise5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ Exercise5/app/src/main/java/com/krish_the_dev/exercise5/MainActivity.java
@@ -0,0 +1,49 @@
+package com.krish_the_dev.exercise5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    NotificationManager nm;
+    NotificationChannel notificationChannel;
+    final String CHANNEL_ID = "krish_the_dev.general";
+
+    private void createNotification(String title, String message) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        nm.notify(1, builder.build());
+
+        Toast.makeText(getApplicationContext(), "Notification created", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nm = (NotificationManager) getSystemService(NotificationManager.class);
+
+        TextView title = findViewById(R.id.notification_title);
+        TextView message = findViewById(R.id.notification_message);
+
+        Button btn = findViewById(R.id.createButton);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notificationChannel = new NotificationChannel(CHANNEL_ID,"General", NotificationManager.IMPORTANCE_DEFAULT);
+            nm.createNotificationChannel(notificationChannel);
+        }
+
+        btn.setOnClickListener(v -> {
+            createNotification(title.getText().toString().trim(), message.getText().toString().trim());
+        });
+    }
+}
