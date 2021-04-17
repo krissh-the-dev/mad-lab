5d2664f188012d2b85ace9ce14269c848ac27b9a
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-04-17 11:55:12 +0530

    integrated google maps API & Location permissions

--- Exercise7/app/src/main/java/com/krish_the_dev/exercise7/MapsActivity.java
+++ Exercise7/app/src/main/java/com/krish_the_dev/exercise7/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.krish_the_dev.exercise7;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,10 +19,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private String[] requiredPermissions = new String[] {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Get location permissions
+        ActivityCompat.requestPermissions(this, requiredPermissions, PackageManager.PERMISSION_GRANTED);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
