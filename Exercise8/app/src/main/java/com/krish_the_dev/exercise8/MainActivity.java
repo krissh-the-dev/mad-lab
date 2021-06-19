e1e58e2e7b7505f156694d4b6bdd00089840a226
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-06-19 08:46:38 +0530

    write file to internal_storage/documents/sample.txt instead app data

--- Exercise8/app/src/main/java/com/krish_the_dev/exercise8/MainActivity.java
+++ Exercise8/app/src/main/java/com/krish_the_dev/exercise8/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -28,13 +29,14 @@ import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
     // Storage Permissions
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final int REQUEST_EXTERNAL_STORAGE = 23;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
-    private static final int BLOCK_SIZE = 100;
+    File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+    File file = new File(folder, "sample.txt");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,42 +59,23 @@ public class MainActivity extends AppCompatActivity {
         Button writeBtn = findViewById(R.id.writeBtn);
         Button clearBtn = findViewById(R.id.clearBtn);
 
-//        String path = Environment.getExternalStorageDirectory().toString() + "/sample.txt";
-        String path = "sample.txt";
-
         readBtn.setOnClickListener(v -> {
             try {
-                FileInputStream fileInputStream = openFileInput(path);
-                InputStreamReader reader = new InputStreamReader(fileInputStream);
-
-                char[] buffer = new char[BLOCK_SIZE];
-                String fileContent = "";
-                int charRead;
-
-                while ((charRead = reader.read(buffer))>0) {
-                    String readString = String.copyValueOf(buffer,0,charRead);
-                    fileContent += readString;
-                }
-                reader.close();
+                String fileContent = readFile(file);
                 textInput.setText(fileContent);
-
                 Toast.makeText(getApplicationContext(), "File read successfully", Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
+            } catch (IOException exp) {
+                exp.printStackTrace();
                 Toast.makeText(getApplicationContext(), "Error reading file", Toast.LENGTH_SHORT).show();
-                e.printStackTrace();
             }
         });
 
         writeBtn.setOnClickListener(v -> {
             try {
                 String message = textInput.getText().toString();
-
-                FileOutputStream fileOut = openFileOutput(path, MODE_PRIVATE);
-                OutputStreamWriter outputWriter = new OutputStreamWriter(fileOut);
-                outputWriter.write(message);
-                outputWriter.close();
-
-                Toast.makeText(getApplicationContext(), "File written successfully", Toast.LENGTH_SHORT).show();
+                writeTextData(file, message);
+                textInput.setText("");
+                Toast.makeText(getApplicationContext(), "File write successful", Toast.LENGTH_SHORT).show();
             } catch (Exception ex) {
                 Toast.makeText(getApplicationContext(), "Error writing file", Toast.LENGTH_SHORT).show();
                 ex.printStackTrace();
@@ -103,5 +86,31 @@ public class MainActivity extends AppCompatActivity {
             textInput.setText("");
             Toast.makeText(getApplicationContext(),"Text cleared", Toast.LENGTH_SHORT).show();
         });
+    }
+
+    private String readFile(File file) throws IOException{
+        if(!file.exists()) {
+            Toast.makeText(getApplicationContext(),
+                    "File does not exist, create one first.",
+                    Toast.LENGTH_SHORT).show();
+            return "";
+        }
+
+        FileReader fileReader = new FileReader(file);
+
+        int byteOfData;
+        String accumulator = "";
+        while ((byteOfData = fileReader.read()) != -1) {
+            accumulator += (char)byteOfData;
+        }
+        fileReader.close();
+        return accumulator;
+        
+    }
+
+    private void writeTextData(File file, String data) throws IOException{
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        fileOutputStream.write(data.getBytes());
+        fileOutputStream.close();
     }
 }
